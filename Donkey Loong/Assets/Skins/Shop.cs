297f17d182dbfaf54d1399fe299c0c6f0428621a
Serhat Pehlivan <serhatholmes9703@hotmail.com>
2021-08-12 18:18:12 +0300

    mark8- shop asset added, controls done,
    v07 alındı, telefonda çalışıyor

--- Donkey Loong/Assets/Skins/Shop.cs
+++ Donkey Loong/Assets/Skins/Shop.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
 
-public class Shop : MonoBehaviour
+public class Store : MonoBehaviour
 {
     public SpriteRenderer sr;
     public List<Sprite> skins = new List<Sprite>();
