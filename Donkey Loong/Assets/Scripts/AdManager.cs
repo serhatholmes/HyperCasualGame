a4b00c1da6a4f2a9f1dbd3f691b7852e0ea42495
Serhat Pehlivan <serhatholmes9703@hotmail.com>
2021-10-11 13:07:56 +0300

    Try another ad

---
+++ Donkey Loong/Assets/Scripts/AdManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IronSourceJSON;
+using UnityEngine.UI;
+
+public class AdManager : MonoBehaviour
+{
+    public Text Rewardedstatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IronSource.Agent.init("116d4c65d", IronSourceAdUnits.REWARDED_VIDEO, IronSourceAdUnits.INTERSTITIAL, IronSourceAdUnits.OFFERWALL, IronSourceAdUnits.BANNER);
+
+        Invoke("InitBanner", 1f);
+        Invoke("FullScreenAd", 2f);
+        
+    }
+
+    void Update()
+    {
+        Rewardedstatus.text = IronSource.Agent.isRewardedVideoAvailable().ToString();
+    }
+
+    //banner
+
+    public void InitBanner()
+    {
+        IronSource.Agent.loadBanner(IronSourceBannerSize.SMART, IronSourceBannerPosition.TOP);
+        
+
+    }
+
+    public void FullScreenAd()
+    {
+        IronSource.Agent.loadInterstitial();
+    }
+
+    public void ShowFullScreenAd()
+    {
+        IronSource.Agent.showInterstitial();
+    }
+
+    public void RewardedAdShow()
+    {
+        IronSource.Agent.showRewardedVideo();
+    }
+}
